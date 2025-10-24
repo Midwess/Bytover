f8bef17956e79d1b533d92e4c0f873324f6b30ab
tiendangkobiton <tiendang@kobiton.com>
2025-10-24 14:54:52 +0700

    Better translucient

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -2,7 +2,6 @@ use std::sync::{Arc, LazyLock};
 use crux_core::Core;
 use tauri::{AppHandle, Emitter, Manager};
 use tokio::{fs, spawn};
-use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 use core_services::logger;
 use native::di_container::DiContainer;
 use shared::app::{AppEvent, AppOperation, AppViewModel};
--- desktop/src/send/window.tsx
+++ desktop/src/send/window.tsx
@@ -13,10 +13,10 @@ function Window() {
   return (
     <main className="w-screen h-screen overflow-hidden p-2 dark bg-transparent">
         <div className={"w-full h-full flex flex-row rounded-2xl bg-transparent space-x-1"}>
-            <div className={"w-full h-full bg-transparent"}>
+            <div className={"flex-6/12 w-full h-full bg-transparent"}>
                 <Shelf/>
             </div>
-            <div className={"w-full h-full bg-transparent"}>
+            <div className={"flex-7/12 w-full h-full bg-transparent"}>
                 <Transfer/>
             </div>
         </div>
