feefbdddcc220b4bf278a564fe729474a819e140
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-10-28 21:41:23 +0700

    Postgres migration (#35)

---
+++ backend/migration/src/model/transfer_session.rs
@@ -0,0 +1,25 @@
+use sea_orm::entity::prelude::*;
+use sea_orm::JsonValue;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "transfer_session")]
+pub struct Model {
+	#[sea_orm(primary_key, auto_increment = false)]
+	pub id: i64,
+	pub alias: String,
+	pub password: Option<String>,
+	#[sea_orm(column_type = "JsonBinary", nullable)]
+	pub to_emails: Option<JsonValue>,
+	pub order_id: i64,
+	pub owner_user_order_id: i64,
+	#[sea_orm(column_type = "JsonBinary", nullable)]
+	pub progress: Option<JsonValue>,
+	#[sea_orm(column_type = "JsonBinary", nullable)]
+	pub resources: Option<JsonValue>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
