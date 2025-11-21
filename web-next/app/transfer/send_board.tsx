d456a25bd9c031f22e231f3567266c15253c0bc4
tiendang <tiendvlp@gmail.com>
2025-11-21 22:22:35 +0700

    fix

--- web-next/app/transfer/send_board.tsx
+++ web-next/app/transfer/send_board.tsx
@@ -185,7 +185,7 @@ function FileSelections() {
                 <div
                     className="absolute top-0 left-0 right-0 h-8 bg-gradient-to-b from-blackBase to-transparent z-10 pointer-events-none"/>
 
-                <div className={"w-full h-full overflow-y-scroll pb-[300px] overflow-x-hidden"}>
+                <div className={"w-full h-full overflow-y-scroll pb-[100px] overflow-x-hidden"}>
                     <div
                         className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-4 pb-8 w-full pt-5 h-fit">
                         {
