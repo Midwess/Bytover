b4bfcef1be54258138805a6d2acbf48e5023aef4
tiendang <tiendvlp@gmail.com>
2025-12-01 19:18:25 +0700

    a

--- web-next/app/home/introduction.tsx
+++ web-next/app/home/introduction.tsx
@@ -5,6 +5,7 @@ import Aurora from "@/components/Aurora";
 import { DownloadPlatforms } from "@/components/download-platforms";
 import Link from "next/link";
 import Image from "next/image";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 interface IntroductionProps {
     disableBackground?: boolean;
@@ -32,9 +33,9 @@ export default function Introduction({
                 <div className={'flex flex-col items-center justify-center gap-12 md:gap-32 container z-2 w-full'}>
                     <div className={"flex flex-col items-center gap-8 md:gap-20"}>
                         <div className={"flex flex-col items-center gap-2 md:gap-4"}>
-                            <div className={"flex flex-row gap-1 md:gap-2 text-2xl md:text-3xl lg:text-5xl font-black text-center items-center"}>
+                            <div className={"flex bg-muted-foreground/10 py-1 px-2 rounded-lg flex-row gap-1 md:gap-2 text-2xl md:text-3xl lg:text-5xl font-black text-center items-center"}>
                                 <Image src={getAssetUrl("/logo-color.svg")} alt={"logo"} width={40} height={40} className={"w-6 h-6 md:w-10 md:h-10"} />
-                                <span className="whitespace-nowrap">Bytover</span>
+                                <span className="whitespace-nowrap text-sm sm:text-md">Bytover</span>
                             </div>
                             <h1
                                 className="text-4xl md:text-5xl lg:text-7xl font-black text-center h1 pointer-events-none px-2"
@@ -58,21 +59,22 @@ export default function Introduction({
 }
 
 export function GravityBackground() {
+    const isMobile = useIsMobile()
     return (
         <>
             <div className="w-screen md:pb-0 h-[95vh] md:h-[80vh] overflow-hidden absolute top-0 left-0 z-1 pointer-events-none">
                 <Aurora
                     // Stronger blue palette
                     colorStops={[
-                        "#00336a", // deep sapphire
-                        "#005fc1", // vibrant blue
-                        "#2176ff", // strong vivid blue
-                        "#2049a8", // royal blue
-                        "#0a2873", // very deep blue
+                        "#1a4779", // deep sapphire, 10% lighter
+                        "#1a6fc7", // vibrant blue, 10% lighter
+                        "#3784ff", // strong vivid blue, 10% lighter
+                        "#365ba1", // royal blue, 10% lighter
+                        "#243f7f", // very deep blue, 10% lighter
                     ]}
-                    blend={0.50}
-                    amplitude={0.15}
-                    speed={1.0}
+                    blend={isMobile ? 0.25 : 0.50}
+                    amplitude={isMobile ? 0.20 : 0.15}
+                    speed={isMobile ? 1.5 : 1.0}
                 />
             </div>
         </>
