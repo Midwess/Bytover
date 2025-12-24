b64eed5c772d50a4505992a91608f987d7c6a979
tiendang <tiendvlp@gmail.com>
2025-12-24 12:39:18 +0700

    Sleep control with rtt

--- shared/src/protocol/webrtc/peer.rs
+++ shared/src/protocol/webrtc/peer.rs
@@ -1132,7 +1132,7 @@ impl WebRtcPeer {
                     hold_counter += 1;
                     hold_counter = hold_counter.min(on_hold_stop_threshold);
                     if hold_counter >= on_hold_slow_threshold {
-                        let _ = sleep_control_tx.try_send(30 * (hold_counter as u64));
+                        let _ = sleep_control_tx.try_send(fec_sender.rtt().min(55).max(15) * (hold_counter as u64));
                     }
 
                     log::info!("Sent hold delimiter, hold_counter increased to {}", hold_counter);
