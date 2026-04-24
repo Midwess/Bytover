58254628edc0f15ef73c4cdbb722369e12ab9591
tiendang <team@midwess.com>
2026-04-24 17:09:06 +0700

    fix this issue for ../app-gateway, it must able to connect to our database hread 'main' (5901303) panicked at src/di.rs:99:85: Failed to connect to Postgres: Connection Error: error returned from database: password authentication failed for user "app_gateway" note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace

--- postgres-init.sql
+++ postgres-init.sql
@@ -1,6 +1,10 @@
 -- Connect to postgres database to create other databases
 \c postgres
 
+-- Create app_gateway login role if it doesn't exist
+SELECT 'CREATE ROLE app_gateway LOGIN PASSWORD ''appgatewaypass'''
+WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'app_gateway')\gexec
+
 -- Create bitbridge database if it doesn't exist
 SELECT 'CREATE DATABASE bitbridge'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bitbridge')\gexec
@@ -9,6 +13,11 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bitbridge')\gexec
 SELECT 'CREATE DATABASE kong'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'kong')\gexec
 
+-- Create app_gateway database owned by app_gateway if it doesn't exist
+SELECT 'CREATE DATABASE app_gateway OWNER app_gateway'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'app_gateway')\gexec
+
 -- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE bitbridge TO bitbridge;
 GRANT ALL PRIVILEGES ON DATABASE kong TO bitbridge;
+GRANT ALL PRIVILEGES ON DATABASE app_gateway TO app_gateway;
